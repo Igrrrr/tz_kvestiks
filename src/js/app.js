67296c3f060116e53623630fdc67cedd09f93dfd
Igor Konovodov <konovodov@list.ru>
2024-10-15 23:34:52 +0300

    fix: sidebar; completed: breadcrumbs; start: slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,19 @@
 import * as bootstrap from "./modules/bootstrap.bundle.min.js";
+
+const slider = new Splide("#slider", {
+  perPage: 4,
+  gap: "1rem",
+  flickMaxPages: 1,
+  perPage: 3,
+  arrows: false,
+  //pagination: false,
+  //gap: 40,
+  breakpoints: {
+    767: {
+      perPage: 1,
+      //pagination: true,
+    },
+  },
+});
+
+slider.mount();
